201ea6ccd2ad8295e332c891fab31baa8f248a2b
Toni Zollo <tonizollo@gmail.com>
2025-11-07 14:08:08 +1000

    fix: debug session 2

--- index.js
+++ index.js
@@ -30,20 +30,13 @@ app.get("/", function (req, res) {
   res.sendFile(__dirname + '/views/index.html');
 });
 
-
-// 2. A request to /api/:date? with a valid date should return a JSON object with a unix key that is a Unix timestamp of the input date 
-// in milliseconds (as type Number)
-// 3. A request to /api/:date? with a valid date should return a JSON object with a utc key that is a string of the input date in the format:
-// Thu, 01 Jan 1970 00:00:00 GMT
-// 4. A request to /api/1451001600000 should return { unix: 1451001600000, utc: "Fri, 25 Dec 2015 00:00:00 GMT" }
 app.get("/api/:date?", function (req, res) {    // "?" makes the date parameter optional
   let unix_int;
   let utc_time;
   let returned_json;
   let valid_input;
-  // 7. An empty date parameter should return the current time in a JSON object with a unix key
-  // 8. An empty date parameter should return the current time in a JSON object with a utc key
-  //console.log('req.params.date is ',req.params.date);
+
+  // Check for no parameter
   if (!req.params.date || req.params.date === '') {
     valid_input = true;
     utc_time = new Date();
@@ -55,14 +48,12 @@ app.get("/api/:date?", function (req, res) {    // "?" makes the date parameter 
     unix_int = parseInt(req.params.date);
     utc_time = new Date(unix_int);
 
-  // 5. Your project can handle dates that can be successfully parsed by new Date(date_string)
   // Check for date type eg 2015-12-256
   } else if (isValidDateString(req.params.date)) { 
     valid_input = true;
     utc_time = new Date(req.params.date);
     unix_int = utc_time.getTime();
 
-  // 6. If the input date string is invalid, the API returns an object having the structure { error : "Invalid Date" }
   // Invalid input
   } else {
     valid_input = false;
@@ -73,6 +64,7 @@ app.get("/api/:date?", function (req, res) {    // "?" makes the date parameter 
   } else {
     returned_json = { "error": "Invalid Date" }
   }
+  console.log(returned_json);
   res.json(returned_json);
 });
 
